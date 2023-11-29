25f16c5128a17f657cda24f3bbc9446db39be0be
WYXNICK <1685952374@qq.com>
2023-11-29 21:12:06 +0800

    初始化ClassmateGalaxy后端项目

---
+++ src/main/java/com/se/classmategalaxy/ClassmateGalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.se.classmategalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmateGalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmateGalaxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/classmategalaxy/ClassmateGalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.classmategalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassmateGalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
